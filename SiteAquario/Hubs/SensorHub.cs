59cdadbc24cf6483f7728b766f516e367051b5d3
Naja Info <Naja Info@NALDINHO>
2024-06-18 00:49:47 -0300

    Adicionar arquivos de projeto.

---
+++ SiteAquario/Hubs/SensorHub.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using Microsoft.EntityFrameworkCore;
+using SiteAquario.Data;
+
+namespace SiteAquario.Hubs
+{
+    public class SensorHub : Hub
+    {
+        private readonly SiteAquarioContext _context;
+
+        public SensorHub(SiteAquarioContext context)
+        {
+            _context = context;
+        }
+
+        public async Task SendData()
+        {
+            var data = await _context.ValoresSensor.ToListAsync();
+            //Console.WriteLine($"Data retrieved: {data.Count} items.");
+            await Clients.All.SendAsync("ReceiveData", data);
+        }
+    }
+}
